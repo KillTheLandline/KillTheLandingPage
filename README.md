4963431a3a6937c19402133a807ea492e7b1a518
Philip I. Thomas <mail@philipithomas.com>
2012-11-28 11:05:35 -0600

    Landing page

---
+++ README.md
@@ -0,0 +1,3 @@
+# Landing Page for Kill The Landline
+
+This is the landing page developed for [Kill The Landline](http://killthelandline.com) at Startup Weekend Columbia 2012, with the help of Jeff Bajayo. 
---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Kill The Landline</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Forward old numbers to your cell">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+   <!-- <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">-->
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-35186330-1']);
+	  _gaq.push(['_setDomainName', 'killthelandline.com']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+  </head>
+
+  <body>
+  <div class="row" style="padding-top: 10px;"><div class="span12"><div class="pull-right"><a class="btn btn-small btn-inverse" href="https://app.killthelandline.com">Sign In</a></div></div></div>
+  <center><a href="/"><img alt="Kill The Landline" src="/assets/img/newLogo.png"></a></center><br /><br /><br />
+  
+ <center><span style="color: #797676; font-size: 24px; line-height: 34px">Don’t pay $40/month for a landline you don’t use.</span></center>
+ 
+ <center><span style="color: #797676; font-size: 20px; line-height: 28px;">Keep your old number and forward<br />
+it to up to two phones.</span></center><br />
+
+<center><img alt="Only $10/month" src="/assets/img/forOnly.png"></center><br />
+
+<form action="https://app.killthelandline.com/signup.php" method="post" accept-charset="UTF-8">
+<center><div class="input-append">
+           <input type="text" id="email" class="span4" name="email" autofocus="autofocus" placeholder="Email . . . ">
+<button class="btn btn-info " type="submit">Sign Up</button>
+</div></center></form>
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  	<hr />
+	<center><small></small>&copy; 2012 <a href="/">Kill The Landline</a>. All rights reserved. Contact us at <a href="mailto:contact@killthelandline.com">Contact@KillTheLandline.com</a>.</center>
+
+
+
+
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+
+  </body>
+</html>
